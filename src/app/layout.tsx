ce86bc47bbb3834d527f988cd3220485ccd2c4d9
leoFagundes <leofagundes2015@gmail.com>
2024-12-26 12:38:03 -0300

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Saira } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const saira = Saira({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body
         className={`${saira.className} flex flex-col items-center antialiased bg-white text-black`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
