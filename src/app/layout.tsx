8ecdcba0f8d6f7d7a84a21a0d4d9e9a005c923b8
leoFagundes <leofagundes2015@gmail.com>
2024-12-26 19:07:31 -0300

    add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Saira } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const saira = Saira({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         className={`${saira.className} flex flex-col items-center antialiased bg-white text-black`}
       >
         <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
